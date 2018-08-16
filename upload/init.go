91345b3f22c00519b660d808cbc8208096bdf4ce
shiruitao <1257201113@qq.com>
2018-08-16 20:07:29 +0800

    init

--- upload/init.go
+++ upload/init.go
@@ -6,28 +6,47 @@
 package upload
 
 import (
-	"os"
 	"database/sql"
+	"os"
 
 	"github.com/TechCatsLab/apix/http/server"
-	"github.com/TechCatsLab/sor/upload/http"
-	"github.com/TechCatsLab/sor/base"
-	"github.com/TechCatsLab/sor/upload/mysql"
 	log "github.com/TechCatsLab/logging/logrus"
+	"github.com/TechCatsLab/sor/base"
+	"github.com/TechCatsLab/sor/base/constants"
+	"github.com/TechCatsLab/sor/upload/http"
+	"github.com/TechCatsLab/sor/upload/mysql"
 )
 
-const (
-	imageFilePath = "./files/image/"
-	videoFilePath = "./files/video/"
-	otherFilePath = "./files/other/"
-)
+func InitRouter(r *server.Router, db *sql.DB, baseUrl, tokenKey string) {
+	if r == nil {
+		log.Fatal("[InitRouter]: server is nil")
+	}
+	//s.AttachMiddleware(middleware.NegroniJwtHandler(tokenKey, Skipper, nil, JwtErrHandler))
 
-func checkDir(path... string) error {
-	for _, name := range path{
-		_, err := os.Stat(name)
+	err := mysql.Create(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = checkDir(constants.PictureDir, constants.VideoDir, constants.OtherDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := &http.UploadController{
+		base.New(db, baseUrl, tokenKey),
+	}
+
+	r.Post("/api/v1/user/upload", c.Upload)
+	r.Post("/api/v1/admin/getbytime", c.QueryByTime)
+	r.Post("/api/v1/admin/getbyuid", c.QueryByUserID)
+}
+
+func checkDir(path ...string) error {
+	for _, name := range path {
+		_, err := os.Stat(constants.FileUploadDir + "/" + name)
 		if err != nil {
 			if os.IsNotExist(err) {
-				err = os.MkdirAll(name, 0777)
+				err = os.MkdirAll(constants.FileUploadDir+"/"+name, 0777)
 				if err != nil {
 					return err
 				}
@@ -35,25 +54,4 @@ func checkDir(path... string) error {
 		}
 	}
 	return nil
-}
-
-func InitRouter(r *server.Router, db *sql.DB, baseUrl string) {
-	if r == nil {
-		panic("[InitRouter]: server is nil")
-	}
-
-	err := mysql.Create(db)
-	if err != nil {
-		log.Warn("Mysql Error:", err)
-	}
-	err = checkDir(imageFilePath, videoFilePath, otherFilePath)
-	if err != nil {
-		log.Warn("mkdir error:")
-	}
-
-	c := &http.UploadController{
-		base.New(db, baseUrl),
-	}
-
-	r.Post("/api/v1/upload", c.Upload)
 }
