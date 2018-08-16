9ad56b8f3cfbc5f933a3c16c301076c3990800d1
shiruitao <1257201113@qq.com>
2018-08-16 20:05:01 +0800

    move const

---
+++ base/constants/const.go
@@ -0,0 +1,33 @@
+/*
+ * Revision History:
+ *     Initial: 2018/08/14        Shi Ruitao
+ */
+
+package constants
+
+const (
+	// UserID - user id
+	UserID = "id"
+	// InvalidUID - userID invalid
+	InvalidUID = 0
+	//InvalidAdmin - admin invalid
+	InvalidAdmin = false
+
+	// RespKeyStatus - json key 'status'
+	RespKeyStatus = "status"
+	// RespKeyData - json key 'data'
+	RespKeyData = "data"
+
+	// fileKry - key of the file
+	FileKey = "file"
+
+	//FileUploadDir - the root directory of the upload files
+	FileUploadDir = "files"
+
+	// PictureDir - save pictures file
+	PictureDir = "picture"
+	// VideoDir - save videos file
+	VideoDir = "video"
+	// OtherDir - files other than video and picture
+	OtherDir = "other"
+)
